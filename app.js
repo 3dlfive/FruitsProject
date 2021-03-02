37115ad23b6fb7d4357a0b2e0af16b477880d20e
3lfive <denystry@gmail.com>
2021-03-02 14:44:09 +0200

    переход к mongoose

--- app.js
+++ app.js
@@ -1,39 +1,23 @@
 const mongoose = require('mongoose');
 
 mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true });
+
 const fruitSchema = new mongoose.Schema ({
   name: String,
   rating: Number,
   review: String
 });
 
+const Fruit = mongoose.model("Fruit", fruitSchema);
 //mongoose создание данных в бд
+const fruit = new Fruit ({
+  name: "Apple",
+  rating: 7,
+  review: "Pretty solid as a fruit"
+});
+fruit.save();
 
 
-
-//Создаёт новую коллекцию
-const insertDocuments = function(db, callback) {
-  // Get the documents collection
-  const collection = db.collection('fruits');
-  // Insert some documents
-  collection.insertMany([
-            {name : "Apple",
-              score: 8,
-              review: "Great fruit"},
-            {name: "Orange",
-              score: 6,
-              review: "Kinda sour"},
-            {name: "Banana",
-              score: 9,
-              review: "Great stuff!"}
-  ], function(err, result) {
-    assert.equal(err, null);
-    assert.equal(3, result.result.n);
-    assert.equal(3, result.ops.length);
-    console.log("Inserted 3 documents into the collection");
-    callback(result);
-  });
-};
 
 //Поиск в коллекции
 const findDocuments = function(db, callback) {
