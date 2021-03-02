fc2837fad72827ccd5fe63fc14f7b9bc3701b45a
3lfive <denystry@gmail.com>
2021-03-02 16:05:04 +0200

    forEach

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const mongoose = require('mongoose');
 
-mongoose.connect("mongodb://localhost:27017/fruitsDB", { useNewUrlParser: true,
-useUnifiedTopology: true  },
-);
+mongoose.connect("mongodb://localhost:27017/fruitsDB", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}, );
 //Создание схемы базы данных
-const fruitSchema = new mongoose.Schema ({
+const fruitSchema = new mongoose.Schema({
   name: String,
   rating: Number,
   review: String
@@ -12,7 +13,7 @@ const fruitSchema = new mongoose.Schema ({
 //Создание модели, происходит связь с схемы и бд
 const Fruit = mongoose.model("Fruit", fruitSchema);
 //mongoose создание данных в бд
-const fruit = new Fruit ({
+const fruit = new Fruit({
   name: "Apple",
   rating: 7,
   review: "Pretty solid as a fruit"
@@ -24,25 +25,28 @@ const peopleSchema = new mongoose.Schema({
   age: Number
 });
 
-const People = mongoose.model("People", peopleSchema);//Создаем модель
-const people = new People ({
+const People = mongoose.model("People", peopleSchema); //Создаем модель
+const people = new People({
   name: "Oleg",
   age: 37
 });
 //people.save();
-const kiwi = new Fruit ({
+const kiwi = new Fruit({
   name: "kiwi",
   rating: 2,
   review: "Good"
-});const orange = new Fruit ({
+});
+const orange = new Fruit({
   name: "orange",
   rating: 7,
   review: "Best fruit"
-});const banana = new Fruit ({
+});
+const banana = new Fruit({
   name: "banana",
   rating: 4,
   review: "Yellow fruit"
-});const peach = new Fruit ({
+});
+const peach = new Fruit({
   name: "peach",
   rating: 10,
   review: "Swetty"
@@ -58,13 +62,14 @@ const kiwi = new Fruit ({
 // });
 
 // Чтение с дб циклом
-Fruit.find(function(err,fruits){
+Fruit.find(function(err, fruits) {
   if (err) {
     console.log(err);
   } else {
-    for (let i = 0; i<fruits.length;i++){
-      console.log(fruits[i].name);
-    }
-
+    fruits.forEach(function(fruit){
+      console.log(fruit.name);
+    });
+    // for (let i = 0; i<fruits.length;i++){
+    //   console.log(fruits[i].name);
   }
 });
